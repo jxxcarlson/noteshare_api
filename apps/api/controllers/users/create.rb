515e66a7c0f4b15525a5c5d298df41f77f54a887
James Carlson <jxxcarlson@gmail.com>
2016-07-19 15:07:12 -0400

    When creating a new user, return an access token

--- apps/api/controllers/users/create.rb
+++ apps/api/controllers/users/create.rb
@@ -1,3 +1,6 @@
+
+require_relative '../../../../lib/xdoc/interactors/grant_access'
+
 module Api::Controllers::Users
   class Create
     include Api::Action
@@ -8,10 +11,12 @@ module Api::Controllers::Users
       # self.body = "username = #{params['username']}, password = #{params['password']}, password_confirmation = #{params['password_confirmation']}, email = #{params['email']} "
       if result.err
         error = result.err[1]
+        self.body = { :status => result.status, :error => error }.to_json
       else
         error = 'None'
+        access= AccessToken.new(username: params[:username], password: params[:password]).call
+        self.body = { :status => result.status, :error => error, :token => access.token }.to_json
       end
-      self.body = { status: result.status, error: error }.to_json
 
     end
   end
