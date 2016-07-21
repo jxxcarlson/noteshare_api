793d2e4b818141b51bb29bd5543ad8d54a85709f
James Carlson <jxxcarlson@gmail.com>
2016-07-20 23:49:20 -0400

    Add Asciidoc renderer

--- Gemfile
+++ Gemfile
@@ -5,12 +5,14 @@ gem 'rake'
 gem 'hanami',       '0.7.3'
 gem 'hanami-model', '~> 0.5'
 
-
 gem 'pg'
 
 gem 'bcrypt'
 gem 'jwt'
 gem 'rack-cors', :require => 'rack/cors'
+
+gem 'asciidoctor-latex'
+gem 'thread_safe'
 
 group :test do
   gem 'minitest'
--- apps/api/controllers/documents/update.rb
+++ apps/api/controllers/documents/update.rb
@@ -1,14 +1,23 @@
+require_relative '../../../../lib/xdoc/interactors/render_asciidoc'
+
 module Api::Controllers::Documents
   class Update
     include Api::Action
 
     def call(params)
+      token = params['token']
+      puts "TOKEN: #{token}"
+      text = params['text']
+      puts "TEXT: #{text}"
+
       id = params['id']
       document = DocumentRepository.find(id)
 
       if document
         document.update_from_hash(params)
-        hash = {'response' => '202 Accepted', 'document' => document.to_hash }
+        document.rendered_text = ::RenderAsciidoc.new(source_text: document.text).call.rendered_text
+        DocumentRepository.update document
+        hash = {'status' => '202', 'document' => document.to_hash }
         self.body = hash.to_json
       else
         self.body = '{ "response" => "500 Server error: document not updated" }'
