26eb96a402a62906d853d7cfe2ef8d5b6aa272b5
James Carlson <jxxcarlson@gmail.com>
2016-07-23 20:04:05 -0400

    In JSON response of create document controller, return "status"

--- apps/api/controllers/documents/create.rb
+++ apps/api/controllers/documents/create.rb
@@ -57,7 +57,7 @@ module Api::Controllers::Documents
       document.owner_id = result.user_id
       created_document = DocumentRepository.create document
       if created_document
-        hash = {'response' => '202 Created', 'document' => created_document.to_hash }
+        hash = {'status' => '202', 'document' => created_document.to_hash }
         self.body = hash.to_json
       else
         self.body = '{ "response" => "500 Server error: document not created" }'
