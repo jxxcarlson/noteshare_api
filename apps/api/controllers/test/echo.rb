0376c842988705b41458014d107b86b810744d40
James Carlson <jxxcarlson@gmail.com>
2016-08-09 03:21:02 -0400

    Create file .env.production and set DB URL for heroku

--- apps/api/controllers/test/echo.rb
+++ apps/api/controllers/test/echo.rb
@@ -3,6 +3,7 @@ module Api::Controllers::Test
     include Api::Action
 
     def call(_params)
+      puts "test requested by host #{request.host}"
       dt = DateTime.now
       reply = { 'date' => dt.strftime("%D"), 'time' => dt.strftime("%H:%M:%S")}.to_json
       self.body = reply
