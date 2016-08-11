85b2e8daa7728503fc1dacd0e37eb911e2f3b577
James Carlson <jxxcarlson@gmail.com>
2016-08-11 16:35:32 -0400

    Put create doument code in separate method

--- apps/api/controllers/users/create.rb
+++ apps/api/controllers/users/create.rb
@@ -5,6 +5,17 @@ module Api::Controllers::Users
   class Create
     include Api::Action
 
+    def create_document(user)
+      document = NSDocument.new(owner_id: user.id, title: 'First Document')
+      document.text = 'This is only a test.'
+      document.kind = 'text'
+      document.public = false
+      text = 'This is only a test'
+      document.rendered_text = RenderAsciidoc.new(source_text: text).call.rendered_text
+      document = DocumentRepository.create document
+      document
+    end
+
     def call(params)
       result = CreateUser.new(params).call
 
@@ -13,7 +24,10 @@ module Api::Controllers::Users
         error = result.err[1]
         self.body = { :status => result.status, :error => error }.to_json
       else
+        user = result.user
         error = 'None'
+        document = create_document(user)
+        puts "DOC: #{document.id}, title: #{document.title}"
         access= AccessToken.new(username: params[:username], password: params[:password]).call
         self.body = { :status => result.status, :error => error, :token => access.token }.to_json
       end
