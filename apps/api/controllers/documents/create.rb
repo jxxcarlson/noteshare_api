463f1dd298ee17881628b8b8c43ec3b379500c3a
James Carlson <jxxcarlson@gmail.com>
2016-08-15 02:43:43 -0400

    Use created document for storing short_hash

--- apps/api/controllers/documents/create.rb
+++ apps/api/controllers/documents/create.rb
@@ -9,11 +9,25 @@ module Api::Controllers::Documents
 
 
 
-    def create_document
+    def create_document(query_string)
       document = NSDocument.new(params)
       document.owner_id = @access.user_id
       document.author_name = @access.username
+
+
       created_document = DocumentRepository.create document
+
+      command, arg = query_string.split('=')
+      if command == 'append' && arg
+        parent_document = DocumentRepository.find arg
+        puts "APPEND TO: #{parent_document.title}"
+        document_list = parent_document.links['documents'] || []
+        puts "Parent document list: #{document_list}"
+        document_list << created_document.short_hash
+        parent_document.links['documents'] = document_list
+        DocumentRepository.update parent_document
+      end
+
       if created_document
         # response.status = 200
         hash = {'status' => 'success', 'document' => created_document.to_hash }
@@ -27,12 +41,15 @@ module Api::Controllers::Documents
 
     def call(params)
 
-      puts "CREATE DOC, QS = #{request.query_string}"
+      puts "request.query_string: #{request.query_string}"
+      query_string =  request.query_string || ""
+
+      puts "query_string: #{query_string}"
 
       verify(params)
 
       if @access.valid
-        create_document
+        create_document(query_string)
       else
         deny_access
       end
